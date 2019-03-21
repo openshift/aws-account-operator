b866855d0dfd84558803010d4295e6337143ffce
James Harrington <james@harrington.net.au>
2019-03-21 15:04:28 -0400

    Add account status to AccountPool

--- pkg/apis/aws/v1alpha1/accountpool_types.go
+++ pkg/apis/aws/v1alpha1/accountpool_types.go
@@ -22,6 +22,9 @@ type AccountPoolStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	PoolSize          int `json:"poolsize"`
+	UnclaimedAccounts int `json:"unclaimedaccounts"`
+	ClaimedAccounts   int `json:"claimedaccounts"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
