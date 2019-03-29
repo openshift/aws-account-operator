e122234b7d1a7034d1ccaa1b009207236989f179
Taylor Fahlman <tfahlman@redhat.com>
2019-03-29 13:25:43 -0400

    Implement conditions status for the Account CRDs

--- pkg/apis/aws/v1alpha1/account_types.go
+++ pkg/apis/aws/v1alpha1/account_types.go
@@ -1,11 +1,28 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AccountStateStatus defines the various status an Account CR can have
+type AccountStateStatus string
+
+const (
+	// AccountStatusRequested const for Requested status
+	AccountStatusRequested AccountStateStatus = "Requested"
+	// AccountStatusClaimed const for Claimed status
+	AccountStatusClaimed AccountStateStatus = "Claimed"
+	// AccountStatusTransfering const for Transfering status
+	accountStatusTransfering AccountStateStatus = "Transfering"
+	// AccountStatusTransfered const for Transfering status
+	accountStatusTransfered AccountStateStatus = "Transfered"
+	// AccountStatusTransferingDeleting const for Deleting status
+	accountStatusDeleting AccountStateStatus = "Deleting"
+)
 
 // AccountSpec defines the desired state of Account
 // +k8s:openapi-gen=true
@@ -24,9 +41,33 @@ type AccountStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
-	Claimed bool   `json:"claimed"`
-	State   string `json:"state"`
+	Claimed    bool               `json:"claimed"`
+	State      string             `json:"state"`
+	Conditions []AccountCondition `json:"conditions"`
 }
+
+// AccountCondition contains details for the current condition of a AWS account
+type AccountCondition struct {
+	// Type is the type of the condition.
+	Type AccountConditionType `json:"type"`
+	// Status is the status of the condition
+	Status corev1.ConditionStatus `json:"status"`
+	// LastProbeTime is the last time we probed the condition.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+	// LastTransitionTime is the laste time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitioNTime,omitempty"`
+	// Reason is a unique, one-word, CamelCase reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+	// Message is a human-readable message indicating details about last transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+}
+
+// AccountConditionType is a valid value for AccountCondition.Type
+type AccountConditionType string
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
