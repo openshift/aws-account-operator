5b0795af6e3704e9a9a0c7bd009c7582cfabb5b6
Naveen Malik <nmalik@redhat.com>
2019-06-14 09:24:41 -0400

    Drop AWS Account limit down to 4500

--- pkg/controller/account/account_controller.go
+++ pkg/controller/account/account_controller.go
@@ -35,7 +35,7 @@ import (
 var log = logf.Log.WithName("controller_account")
 
 const (
-	awsLimit                = 4900
+	awsLimit                = 4500
 	awsCredsUserName        = "aws_user_name"
 	awsCredsSecretIDKey     = "aws_access_key_id"
 	awsCredsSecretAccessKey = "aws_secret_access_key"
