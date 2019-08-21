137dfed907e0d30d15e361950bed675f0b7db106
Maor Friedman <maor.friedman@redhat.com>
2019-08-21 11:56:08 +0300

    add yaml validation to pr_check

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -4,6 +4,13 @@
 
 set -exv
 
+curl -s https://raw.githubusercontent.com/maorfr/utilities/master/validate_yaml.py > validate_yaml.py
+python validate_yaml.py deploy/crds
+if [ "$?" != "0" ]; then
+    exit 1
+fi
+rm validate_yaml.py
+
 BASE_IMG="aws-account-operator"
 IMG="${BASE_IMG}:latest"
 
