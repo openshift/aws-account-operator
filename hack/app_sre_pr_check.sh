265476893ebb138e592a7eab0eae2a1eaa2bd50b
Maor Friedman <maor.friedman@redhat.com>
2019-08-22 09:03:20 +0300

    add validate_yaml script to repo

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -4,12 +4,12 @@
 
 set -exv
 
-curl -s https://raw.githubusercontent.com/maorfr/utilities/master/validate_yaml.py > validate_yaml.py
-python validate_yaml.py deploy/crds
+CURRENT_DIR=$(dirname "$0")
+
+python "$CURRENT_DIR"/validate_yaml.py "$CURRENT_DIR"/../deploy/crds
 if [ "$?" != "0" ]; then
     exit 1
 fi
-rm validate_yaml.py
 
 BASE_IMG="aws-account-operator"
 IMG="${BASE_IMG}:latest"
