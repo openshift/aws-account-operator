5808ef6f7821515c6b4dbaad100cee4f3c5cd51f
Rogerio Bastos <rogbas@redhat.com>
2019-09-03 10:14:26 -0400

    SREP-2029 Increase account limit to 4550

--- pkg/controller/account/account_controller.go
+++ pkg/controller/account/account_controller.go
@@ -36,7 +36,7 @@ import (
 var log = logf.Log.WithName("controller_account")
 
 const (
-	awsLimit                = 4450
+	awsLimit                = 4550
 	awsCredsUserName        = "aws_user_name"
 	awsCredsSecretIDKey     = "aws_access_key_id"
 	awsCredsSecretAccessKey = "aws_secret_access_key"
