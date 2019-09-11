ce3576186b1b5a8fde0ab4cf0ce255f40ba12e24
Sam Nguyen <khnguyen@redhat.com>
2019-09-11 15:36:03 -0400

    check status error when setStatusFailed

--- pkg/controller/account/account_controller.go
+++ pkg/controller/account/account_controller.go
@@ -588,10 +588,14 @@ func (r *ReconcileAccount) BuildIAMUser(reqLogger logr.Logger, awsClient awsclie
 	return userSecret.ObjectMeta.Name, nil
 }
 
-func (r *ReconcileAccount) setStatusFailed(reqLogger logr.Logger, awsAccount *awsv1alpha1.Account, message string) {
+func (r *ReconcileAccount) setStatusFailed(reqLogger logr.Logger, awsAccount *awsv1alpha1.Account, message string) error {
 	reqLogger.Info(message)
 	awsAccount.Status.State = "Failed"
-	_ = r.Client.Status().Update(context.TODO(), awsAccount)
+	err := r.Client.Status().Update(context.TODO(), awsAccount)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // CreateAccount creates an AWS account for the specified accountName and accountEmail in the orgnization
