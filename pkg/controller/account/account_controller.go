ad7296967595dd351765a8f4ee0833706e785348
Christopher Collins <collins.christopher@gmail.com>
2019-12-04 15:33:34 -0500

    Adds comment to exported variable AwsLimit
    Adds a comment to exported variable AwsLimit to conform with Go linting best practices.
    
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- pkg/controller/account/account_controller.go
+++ pkg/controller/account/account_controller.go
@@ -35,7 +35,8 @@ import (
 
 var log = logf.Log.WithName("controller_account")
 
-const (
+const
+	// Exported and used in cmd/manager/main.go
 	AwsLimit                = 4700
 	awsCredsUserName        = "aws_user_name"
 	awsCredsSecretIDKey     = "aws_access_key_id"
@@ -376,7 +377,7 @@ func (r *ReconcileAccount) Reconcile(request reconcile.Request) (reconcile.Resul
 			}
 
 			// Case not Resolved, log info and try again in pre-defined interval
-			reqLogger.Info(fmt.Sprintf(`Case %s not resolved, 
+			reqLogger.Info(fmt.Sprintf(`Case %s not resolved,
 			trying again in %d minutes`,
 				currentAcctInstance.Status.SupportCaseID,
 				intervalBetweenChecksMinutes))
