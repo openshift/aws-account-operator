df71dcdf1d84b3588cd29ab0f098b27a6b82e751
Christopher Collins <collins.christopher@gmail.com>
2020-10-27 13:28:35 -1000

    fixed accountclaim_test.go; v0.11.x
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- pkg/controller/accountclaim/accountclaim_controller_test.go
+++ pkg/controller/accountclaim/accountclaim_controller_test.go
@@ -115,7 +115,7 @@ var _ = Describe("AccountClaim", func() {
 			}
 
 			objs := []runtime.Object{accountClaim, account}
-			r.client = &tpossiblyErroringFakeCtrlRuntimeClient{
+			r.client = &possiblyErroringFakeCtrlRuntimeClient{
 				fake.NewFakeClient(objs...),
 				true,
 			}
@@ -152,7 +152,8 @@ type possiblyErroringFakeCtrlRuntimeClient struct {
 
 func (p *possiblyErroringFakeCtrlRuntimeClient) Update(
 	ctx context.Context,
-	acc runtime.Object) error {
+	acc runtime.Object,
+	opts ...client.UpdateOption) error {
 	if p.shouldError {
 		return fixtures.Conflict
 	}
