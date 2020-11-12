d8e726787b31100081c99b277fa0a73c77aa959d
Christopher Collins <collins.christopher@gmail.com>
2020-11-12 10:19:13 -1000

    Upgrades operator-sdk to v0.13.x
    This commit upgrades the operator-sdk verstion to v0.13.x
    
    REF: [Upgrade aws-account-operator to sdk
    v0.16.x](https://issues.redhat.com/browse/OSD-5097)
    
    Includes changes introduced in #501, #500 & #499
    
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- pkg/controller/accountpool/accountpool_controller_test.go
+++ pkg/controller/accountpool/accountpool_controller_test.go
@@ -111,6 +111,10 @@ func TestReconcileAccountPool(t *testing.T) {
 				},
 			},
 			expectedAccountPool: awsv1alpha1.AccountPool{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "AccountPool",
+					APIVersion: "aws.managed.openshift.io/v1alpha1",
+				},
 				ObjectMeta: metav1.ObjectMeta{
 
 					Name:      "test",
@@ -144,6 +148,10 @@ func TestReconcileAccountPool(t *testing.T) {
 				},
 			},
 			expectedAccountPool: awsv1alpha1.AccountPool{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "AccountPool",
+					APIVersion: "aws.managed.openshift.io/v1alpha1",
+				},
 				ObjectMeta: metav1.ObjectMeta{
 
 					Name:      "test",
@@ -175,9 +183,16 @@ func TestReconcileAccountPool(t *testing.T) {
 			}
 
 			ap := awsv1alpha1.AccountPool{}
-			err := mocks.fakeKubeClient.Get(context.TODO(), types.NamespacedName{Name: "test", Namespace: "test"}, &ap)
+			err := mocks.fakeKubeClient.Get(
+				context.TODO(),
+				types.NamespacedName{
+					Name:      "test",
+					Namespace: "aws-account-operator",
+				},
+				&ap,
+			)
 			if err != nil {
-				fmt.Printf("Failed returning mock accountPool in accountpool controller tests")
+				fmt.Printf("Failed returning mock accountPool in accountpool controller tests: %s\n", err)
 			}
 
 			_, err = rap.Reconcile(reconcile.Request{
@@ -199,10 +214,12 @@ func verifyAccountPool(c client.Client, expected *awsv1alpha1.AccountPool) bool 
 	err := c.Get(context.TODO(), types.NamespacedName{Name: expected.Name, Namespace: expected.Namespace}, &ap)
 
 	if err != nil {
+		fmt.Printf("Error returning fakeclient accountPool: %s\n", err)
 		return false
 	}
 
 	if !reflect.DeepEqual(ap, *expected) {
+		fmt.Printf("Error comparing accountPool objects.\n\tExpected: %+v\n\tGot: %+v", *expected, ap)
 		return false
 	}
 
