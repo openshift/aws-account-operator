64b7c8a9ec2a8cba02c820c3041c47cc09215a83
Eric Fried <efried@redhat.com>
2021-02-01 15:20:42 -0600

    Fix `make coverage`
    Adds a workaround for https://github.com/golang/go/issues/27333 to make
    `make coverage` work.

---
+++ test/integration/issue27333.go
@@ -0,0 +1,9 @@
+package federatedaccesstesting
+
+/*
+This file exists to work around https://github.com/golang/go/issues/27333
+
+Without it, `make coverage` will fail with:
+
+go build github.com/openshift/aws-account-operator/test/integration: no non-test Go files in /home/efried/go/src/github.com/openshift/aws-account-operator/test/integration
+*/
