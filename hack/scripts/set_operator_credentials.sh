19a7bcd998bd4c6479b71c1c9a76fd7097bc1abc
James Harrington <james@harrington.net.au>
2021-02-17 16:57:51 -0500

    Fix credentials script path

--- hack/scripts/set_operator_credentials.sh
+++ hack/scripts/set_operator_credentials.sh
@@ -20,4 +20,4 @@ ID="$(awk -F " " '($1=="aws_access_key_id") {printf "%s", $3}' <<< "$rawCredenti
 SECRET="$(awk -F " " '($1=="aws_secret_access_key") {printf "%s", $3}' <<< "$rawCredentials" | base64)"
 
 echo "Deploying AWS Account Operator Credentials using AWS Profile $profile"
-oc process -p OPERATOR_ACCESS_KEY_ID=${ID} -p OPERATOR_SECRET_ACCESS_KEY=${SECRET} -p OPERATOR_NAMESPACE=aws-account-operator -f hack/templates/aws_v1alpha1_aws_account_operator_credentials.tmpl | oc apply -f - 
+oc process -p OPERATOR_ACCESS_KEY_ID=${ID} -p OPERATOR_SECRET_ACCESS_KEY=${SECRET} -p OPERATOR_NAMESPACE=aws-account-operator -f hack/templates/aws.managed.openshift.io_v1alpha1_aws_account_operator_credentials.tmpl | oc apply -f -
