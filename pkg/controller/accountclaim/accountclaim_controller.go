37e92b553f86aea5e96573a542b177ffb23144d2
Pavlos Ratis <pratis@redhat.com>
2021-04-12 19:09:49 +0200

    add BYOC account status in log message

--- pkg/controller/accountclaim/accountclaim_controller.go
+++ pkg/controller/accountclaim/accountclaim_controller.go
@@ -223,7 +223,7 @@ func (r *ReconcileAccountClaim) Reconcile(request reconcile.Request) (reconcile.
 				// Update the status on AccountClaim
 				return reconcile.Result{}, r.statusUpdate(reqLogger, accountClaim)
 			}
-			waitMsg := fmt.Sprintf("%s is not Ready yet requeuing in %d seconds", byocAccount.Name, waitPeriod)
+			waitMsg := fmt.Sprintf("%s is not Ready yet (current status: %s) requeuing in %d seconds", byocAccount.Name, byocAccount.Status.State, waitPeriod)
 			reqLogger.Info(waitMsg)
 			return reconcile.Result{RequeueAfter: time.Second * waitPeriod}, nil
 		}
