cb2ceb82ecf3d9cd12e1432d6a1049e95b01afe6
Panagiotis Georgiadis <pgeorgia@redhat.com>
2021-04-29 05:17:05 +0200

    Delete the account before creating it

--- test/integration/api/create_account.sh
+++ test/integration/api/create_account.sh
@@ -3,6 +3,10 @@
 # Load Environment vars
 source hack/scripts/test_envs 
 
+# Delete Account CR (if it exists)
+echo "Delete account if exists"
+oc delete accounts ${OSD_STAGING_1_ACCOUNT_CR_NAME_OSD} -n ${NAMESPACE}
+
 # Create Account CR 
 oc process -p AWS_ACCOUNT_ID=${OSD_STAGING_1_AWS_ACCOUNT_ID} -p ACCOUNT_CR_NAME=${OSD_STAGING_1_ACCOUNT_CR_NAME_OSD} -p NAMESPACE=${NAMESPACE} -f hack/templates/aws.managed.openshift.io_v1alpha1_account.tmpl | oc apply -f -
 # Wait for account to become ready
