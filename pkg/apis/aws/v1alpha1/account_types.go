77ca765f269b9cf77d76d14127b57a1e755afbc7
Kirk Bater <kirk.bater@gmail.com>
2021-08-11 15:55:33 -0400

    Revert "Revert "Update boilerplate and generate v1 CRDs""

--- pkg/apis/aws/v1alpha1/account_types.go
+++ pkg/apis/aws/v1alpha1/account_types.go
@@ -31,9 +31,6 @@ const (
 // AccountSpec defines the desired state of Account
 // +k8s:openapi-gen=true
 type AccountSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 	AwsAccountID  string `json:"awsAccountID"`
 	IAMUserSecret string `json:"iamUserSecret"`
 	BYOC          bool   `json:"byoc,omitempty"`
@@ -48,14 +45,11 @@ type AccountSpec struct {
 // AccountStatus defines the observed state of Account
 // +k8s:openapi-gen=true
 type AccountStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 	Claimed       bool   `json:"claimed,omitempty"`
 	SupportCaseID string `json:"supportCaseID,omitempty"`
 	// +listType=map
 	// +listMapKey=type
-	Conditions               []AccountCondition `json:"conditions,omitempty"`
+	Conditions               []AccountCondition `json:"conditions"`
 	State                    string             `json:"state,omitempty"`
 	RotateCredentials        bool               `json:"rotateCredentials,omitempty"`
 	RotateConsoleCredentials bool               `json:"rotateConsoleCredentials,omitempty"`
@@ -65,7 +59,7 @@ type AccountStatus struct {
 // AccountCondition contains details for the current condition of a AWS account
 type AccountCondition struct {
 	// Type is the type of the condition.
-	Type AccountConditionType `json:"type,omitempty"`
+	Type AccountConditionType `json:"type"`
 	// Status is the status of the condition
 	Status corev1.ConditionStatus `json:"status,omitempty"`
 	// LastProbeTime is the last time we probed the condition.
