2c9390ccfc39d38c5984d1eeaf13144331cd2f38
Dan Keohane <dkeohane@redhat.com>
2021-09-20 17:54:50 +0200

    Update create account script to stop nuking existing account

--- test/integration/api/create_account.sh
+++ test/integration/api/create_account.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # Load Environment vars
-source hack/scripts/test_envs 
+source hack/scripts/test_envs
+
+ACCOUNT_CR_EXISTS=$(oc get account ${OSD_STAGING_1_ACCOUNT_CR_NAME_OSD} -n ${NAMESPACE})
+ACCOUNT_CR_EXISTS=$?
+
+if [[ ${ACCOUNT_CR_EXISTS} = 0 ]]; then
+    echo "Account ${OSD_STAGING_1_ACCOUNT_CR_NAME_OSD} already exists, skipping creation."
+    exit 0
+else
+    echo "Creating Account ${OSD_STAGING_1_ACCOUNT_CR_NAME_OSD}" 
+fi
 
 # Create Account CR 
 oc process -p AWS_ACCOUNT_ID=${OSD_STAGING_1_AWS_ACCOUNT_ID} -p ACCOUNT_CR_NAME=${OSD_STAGING_1_ACCOUNT_CR_NAME_OSD} -p NAMESPACE=${NAMESPACE} -f hack/templates/aws.managed.openshift.io_v1alpha1_account.tmpl | oc apply -f -
