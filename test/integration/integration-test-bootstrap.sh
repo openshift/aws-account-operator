319289db9d8db53664048baf39e2b60f3e3c8f41
Alex Vulaj <avulaj@redhat.com>
2022-10-12 14:29:38 -0400

    Attempts to clean up bootstrap script and fix a bug or two

--- test/integration/integration-test-bootstrap.sh
+++ test/integration/integration-test-bootstrap.sh
@@ -57,7 +57,7 @@ function parseArgs {
                 ;;
         esac
     done
-    if [ -z $NAMESPACE ]; then
+    if [ -z "$NAMESPACE" ]; then
         NAMESPACE=aws-account-operator
     fi
     echo "PROFILE=${PROFILE}"
@@ -73,22 +73,40 @@ function sourceEnvrcConfig {
     source .envrc
 }
 
+function sourceSetOperatorCredentials {
+    if [ ! -f "./hack/scripts/set_operator_credentials.sh" ]; then
+        echo "ERROR - set_operator_credentials.sh does not exist"
+        return 1
+    fi
+    source ./hack/scripts/set_operator_credentials.sh osd-staging-1
+}
+
 function sourceFromMountedKvStoreConfig {
     ## Prow CI uses existing secrets:- https://docs.ci.openshift.org/docs/how-tos/adding-a-new-secret-to-ci/
-    export OSD_STAGING_1_AWS_ACCOUNT_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_1_AWS_ACCOUNT_ID)
-    export OSD_STAGING_2_AWS_ACCOUNT_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_2_AWS_ACCOUNT_ID)
-    export STS_ROLE_ARN=$(cat /tmp/secret/aao-aws-creds/STS_ROLE_ARN)
-    export STS_JUMP_ARN=$(cat /tmp/secret/aao-aws-creds/STS_JUMP_ARN)
-    export OSD_STAGING_1_OU_ROOT_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_1_OU_ROOT_ID)
-    export OSD_STAGING_1_OU_BASE_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_1_OU_BASE_ID)
-    export SUPPORT_JUMP_ROLE=$(cat /tmp/secret/aao-aws-creds/SUPPORT_JUMP_ROLE)
-    export STS_JUMP_ROLE=$(cat /tmp/secret/aao-aws-creds/STS_JUMP_ROLE)
-    export OPERATOR_ACCESS_KEY_ID=$(cat /tmp/secret/aao-aws-creds/OPERATOR_ACCESS_KEY_ID)
-    export OPERATOR_SECRET_ACCESS_KEY=$(cat /tmp/secret/aao-aws-creds/OPERATOR_SECRET_ACCESS_KEY)
+    OSD_STAGING_1_AWS_ACCOUNT_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_1_AWS_ACCOUNT_ID)
+    export OSD_STAGING_1_AWS_ACCOUNT_ID
+    OSD_STAGING_2_AWS_ACCOUNT_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_2_AWS_ACCOUNT_ID)
+    export OSD_STAGING_2_AWS_ACCOUNT_ID
+    STS_ROLE_ARN=$(cat /tmp/secret/aao-aws-creds/STS_ROLE_ARN)
+    export STS_ROLE_ARN
+    STS_JUMP_ARN=$(cat /tmp/secret/aao-aws-creds/STS_JUMP_ARN)
+    export STS_JUMP_ARN
+    OSD_STAGING_1_OU_ROOT_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_1_OU_ROOT_ID)
+    export OSD_STAGING_1_OU_ROOT_ID
+    OSD_STAGING_1_OU_BASE_ID=$(cat /tmp/secret/aao-aws-creds/OSD_STAGING_1_OU_BASE_ID)
+    export OSD_STAGING_1_OU_BASE_ID
+    SUPPORT_JUMP_ROLE=$(cat /tmp/secret/aao-aws-creds/SUPPORT_JUMP_ROLE)
+    export SUPPORT_JUMP_ROLE
+    STS_JUMP_ROLE=$(cat /tmp/secret/aao-aws-creds/STS_JUMP_ROLE)
+    export STS_JUMP_ROLE
+    OPERATOR_ACCESS_KEY_ID=$(cat /tmp/secret/aao-aws-creds/OPERATOR_ACCESS_KEY_ID)
+    export OPERATOR_ACCESS_KEY_ID
+    OPERATOR_SECRET_ACCESS_KEY=$(cat /tmp/secret/aao-aws-creds/OPERATOR_SECRET_ACCESS_KEY)
+    export OPERATOR_SECRET_ACCESS_KEY
 }
 
 function sanityCheck {
-    if [ -z OPERATOR_ACCESS_KEY_ID -o -z OPERATOR_SECRET_ACCESS_KEY ]; then
+    if [ -z "$OPERATOR_ACCESS_KEY_ID" ] || [ -z "$OPERATOR_SECRET_ACCESS_KEY" ]; then
         echo "ERROR: AWS credential variable OPERATOR_ACCESS_KEY_ID or OPERATOR_SECRET_ACCESS_KEY is missing."
         return 1
     fi
@@ -101,9 +119,9 @@ function sanityCheck {
 }
 
 function consoleOperatorLogs {
-    if [ $LOCAL_LOG_FILE -a -f $LOCAL_LOG_FILE ];then
+    if [ "$LOCAL_LOG_FILE" ] && [ -f "$LOCAL_LOG_FILE" ]; then
         echo -e "\nOPERATOR LOGS\n"
-        cat $LOCAL_LOG_FILE
+        cat "$LOCAL_LOG_FILE"
     fi
     getOperatorPodCommand="$OC_WITH_NAMESPACE get po -lname=aws-account-operator"
     if [[ $($getOperatorPodCommand --no-headers | wc -l) == 0 ]];
@@ -115,7 +133,7 @@ function consoleOperatorLogs {
 	echo -e "\nStatus of the operator pod\n"
 	$OC_WITH_NAMESPACE get po "$operatorPodName" -ojsonpath='{.status}'
 	echo -e "\nOPERATOR LOGS\n"
-	$OC_WITH_NAMESPACE logs $operatorPodName
+	$OC_WITH_NAMESPACE logs "$operatorPodName"
 }
 
 function removeDockerfileSoftLink {
@@ -139,11 +157,11 @@ function cleanKustomization {
 function cleanup {
     removeDockerfileSoftLink
     cleanKustomization
-    if [ $localOperatorPID ]; then
-        kill $localOperatorPID 2>/dev/null || true
+    if [ "$localOperatorPID" ]; then
+        kill "$localOperatorPID" 2>/dev/null || true
     fi
-    if [ $LOCAL_LOG_FILE -a -f $LOCAL_LOG_FILE ]; then
-        rm $LOCAL_LOG_FILE
+    if [ "$LOCAL_LOG_FILE" ] && [ -f "$LOCAL_LOG_FILE" ]; then
+        rm "$LOCAL_LOG_FILE"
     fi
     if [ -z $SKIP_CLEANUP ]; then
         $OC_WITH_NAMESPACE delete deployment $OPERATOR_DEPLOYMENT 2>/dev/null || true
@@ -171,8 +189,8 @@ function cleanupPost {
     echo "= After All Test Cleanup"
     echo "========================================================================"
     consoleOperatorLogs
-    if [ $clusterUserName ]; then
-        $OC --as backplane-cluster-admin adm policy remove-cluster-role-from-user cluster-admin $clusterUserName
+    if [ "$clusterUserName" ]; then
+        $OC --as backplane-cluster-admin adm policy remove-cluster-role-from-user cluster-admin "$clusterUserName"
     fi
     cleanup
     echo -e "\nSTARTING AWS RESOURCES CLEANUP FOR CI\n"
@@ -203,7 +221,7 @@ function verifyBuildSuccess {
         echo "ERROR - build was not completed fully, the state was $phase but expected to be 'Complete'"
         echo "the logs for the failed job are:"
 
-        $OC_WITH_NAMESPACE logs $latestJobName-build
+        $OC_WITH_NAMESPACE logs "$latestJobName"-build
         return 1
     fi
 }
@@ -285,6 +303,7 @@ function profileLocal {
     export LOCAL_LOG_FILE="localOperator.log"
     export FORCE_DEV_MODE=local
     sourceEnvrcConfig
+    sourceSetOperatorCredentials
     sanityCheck
 
     echo "Configuring deployment cluster"
@@ -293,15 +312,14 @@ function profileLocal {
     make predeploy
 
     # start local operator if not already running
-    # not sure if theres a better way to detect this, but operator-sdk processes look 
-    # like this when I was testing locally: 
-    #   > ps aux | grep go-build
-    #      mstratto 3313211  0.2  0.1 2590492 117236 pts/7  Sl+  10:59   0:08 /tmp/go-build3762679696/b001/exe/main --zap-devel
-    ps aux | grep "[g]o-build"
-    if [ $? -ne 0 ]; then
+    # operator-sdk processes look like this when I was testing locally:
+    #   > pgrep -f "[g]o-build"
+    #   3313211
+    pgrep -f "[g]o-build"
+    if ! pgrep -f "[g]o-build"; then
         echo "Building and deploying operator image"
         make deploy-local OPERATOR_NAMESPACE=$NAMESPACE > $LOCAL_LOG_FILE 2>&1 &
-        $localOperatorPID=$!
+        localOperatorPID=$!
     fi
 }
 
@@ -334,6 +352,7 @@ function profileStage {
     echo "Configuring local build environment"
     export FORCE_DEV_MODE=cluster
     sourceEnvrcConfig
+    sourceSetOperatorCredentials
     sanityCheck
     
     echo "Configuring depoloyment cluster"
@@ -341,7 +360,7 @@ function profileStage {
     ## OSD Staging cluster require cluster-admin roles for accessing & applying some manifests like CRDs etc.
     ## So, cluster-admin role is added to the user for the script's lifetime.
     ## The role is removed as the part of mandatory cleanup.
-    $OC --as backplane-cluster-admin adm policy add-cluster-role-to-user cluster-admin $clusterUserName
+    $OC --as backplane-cluster-admin adm policy add-cluster-role-to-user cluster-admin "$clusterUserName"
     cleanupPre
     $OC adm new-project "$NAMESPACE" 2>/dev/null || true
     make prow-ci-predeploy
@@ -367,10 +386,10 @@ function explainExitCode {
     local exitCode=$2
     message="UNKNOWN"
 
-    if [ -z $exitCode ]; then
+    if [ -z "$exitCode" ]; then
         message="No test results found"
-    elif [ $exitCode -ne 0 ]; then
-        message=$(/usr/bin/env bash $script explain $exitCode)
+    elif [ "$exitCode" -ne 0 ]; then
+        message=$(/usr/bin/env bash "$script" explain "$exitCode")
         if [ -z "$message" ]; then
             # check the general exit code messages
             message=${GENERAL_EXIT_CODE_MESSAGES[$exitCode]}
@@ -393,19 +412,19 @@ function runTest {
     echo "= Test: $testScript"
     echo "= Phase: setup"
     echo "========================================================================"
-    /usr/bin/env bash $testScript "setup"
+    /usr/bin/env bash "$testScript" "setup"
     setupExitCode=$?
-    setupExitMessage=$(explainExitCode $testScript $setupExitCode)
+    setupExitMessage=$(explainExitCode "$testScript" $setupExitCode)
 
     echo -e "\n========================================================================"
     echo "= Test: $testScript"
     echo "= Phase: test"
     echo "========================================================================"
-    if [ $setupExitCode -eq $EXIT_PASS ]; then
-        /usr/bin/env bash $testScript "test"
+    if [ $setupExitCode -eq "$EXIT_PASS" ]; then
+        /usr/bin/env bash "$testScript" "test"
         testExitCode=$?
-        testExitMessage=$(explainExitCode $testScript $testExitCode)
-        if [ $testExitCode -eq $EXIT_PASS ]; then
+        testExitMessage=$(explainExitCode "$testScript" $testExitCode)
+        if [ $testExitCode -eq "$EXIT_PASS" ]; then
             overall="PASS"
         else
             overall="FAIL"
@@ -421,9 +440,9 @@ function runTest {
     echo "= Test: $testScript"
     echo "= Phase: cleanup"
     echo "========================================================================"
-    /usr/bin/env bash $testScript "cleanup"
+    /usr/bin/env bash "$testScript" "cleanup"
     cleanupExitCode=$?
-    cleanupExitMessage=$(explainExitCode $testScript $cleanupExitCode)
+    cleanupExitMessage=$(explainExitCode "$testScript" $cleanupExitCode)
 
     testResults[$testScript]=$(cat <<EOF
 {
@@ -449,13 +468,13 @@ function printTestResults {
     allPassed=1
     for t in "${!testResults[@]}"; do
         resultJson=${testResults[$t]}
-        testExitCode=$(echo $resultJson | jq -r .testExitCode)
-        overall=$(echo $resultJson | jq -r .overall)
-        setupExitMessage=$(echo $resultJson | jq -r .setupExitMessage)
-        testExitMessage=$(echo $resultJson | jq -r .testExitMessage)
-        cleanupExitMessage=$(echo $resultJson | jq -r .cleanupExitMessage)
+        testExitCode=$(echo "$resultJson" | jq -r .testExitCode)
+        overall=$(echo "$resultJson" | jq -r .overall)
+        setupExitMessage=$(echo "$resultJson" | jq -r .setupExitMessage)
+        testExitMessage=$(echo "$resultJson" | jq -r .testExitMessage)
+        cleanupExitMessage=$(echo "$resultJson" | jq -r .cleanupExitMessage)
 
-        if [ $testExitCode -ne 0 ]; then
+        if [ "$testExitCode" -ne 0 ]; then
             allPassed=0
         fi
 
