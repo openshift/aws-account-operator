ade47bfbd99e5614c7fe38b1cea1403029d66f75
Cortney Reed <creed@redhat.com>
2025-08-29 16:31:03 -0400

    update logging verbosity in accountclaim controller

--- main.go
+++ main.go
@@ -85,9 +85,8 @@ func main() {
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 
-	isDebuggingEnabled := utils.GetEnvironmentBool("DEBUG_LOGGING", false)
 	opts := zap.Options{
-		Development: isDebuggingEnabled,
+		Development: false,
 	}
 	if utils.DetectDevMode == utils.DevModeLocal {
 		zap.UseDevMode(true)
